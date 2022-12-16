f053181ff6a60fc6c649a80e37e905e423ecf23a
Edgeeee11 <arsenykurenkov@gmail.com>
2022-12-16 20:30:48 +0300

    Homework

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+
+main()
+{
+    int mas[100][100];
+
+    int r, c;
+    
+        printf("ROWS");
+        scanf("%i",&r);
+    
+        printf("COLS");
+        scanf("%i",&c);
+
+    
+    printf("vvedite znacheniya dvumernogo massiva cherez Enter\n");
+    
+    for (int j=0; j<r; j++)
+    {
+        for(int i=0; i<c; i++)
+        {
+            scanf("%i",&mas[j][i]);
+        }
+    }
+   
+    printf("Starting position\n");
+
+    for (int j=0; j<r; j++)
+    {
+        for(int i=0; i<c; i++)
+        {
+            printf("%3i",mas[j][i]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    
+    int mas2[100][100];
+
+    for (int j=0; j<r; j++)
+    {
+        for(int i=0; i<c; i++)
+        {
+            mas2[j][i]=mas[i][j];
+        }
+    }
+    
+    printf("Inversion\n");
+
+    for (int j=0; j<c; j++)
+    {
+        for(int i=0; i<r; i++)
+        {
+            printf("%3i",mas2[j][i]);
+        }
+        printf("\n");
+    }
+    
+}
